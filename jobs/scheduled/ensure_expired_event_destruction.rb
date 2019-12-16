c10efc3c6f34d9f7c84a5eeb8506a2dddf156c22
Martin Brennan <martin@discourse.org>
2019-12-16 16:51:27 +1000

    Fixes for deleting post reply logic

--- jobs/scheduled/ensure_expired_event_destruction.rb
+++ jobs/scheduled/ensure_expired_event_destruction.rb
@@ -42,8 +42,9 @@ module Jobs
             post.post_replies.each do |post_reply|
               # we do not want to destroy any direct replies with dates
               # (they will get destroyed automatically when their time comes)
-              next if post_numbers_with_dates.include?(post_reply.reply_id)
-              destroy_post(post_reply.post)
+              reply_post = post_reply.reply
+              next if post_numbers_with_dates.include?(reply_post.post_number)
+              destroy_post(reply_post)
             end
           end
         end
