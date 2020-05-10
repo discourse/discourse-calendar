b5c79104215d8bb52b12c9ba6fb294dacbdb6edb
jjaffeux <j.jaffeux@gmail.com>
2020-05-11 00:05:17 +0200

    FIX: do not ensure logged in
    Right are made at the routes level.

--- app/controllers/discourse_post_event_controller.rb
+++ app/controllers/discourse_post_event_controller.rb
@@ -2,8 +2,6 @@
 
 module DiscoursePostEvent
   class DiscoursePostEventController < ::ApplicationController
-    before_action :ensure_logged_in
-
     before_action :ensure_discourse_post_event_enabled
     def ensure_discourse_post_event_enabled
       if !SiteSetting.discourse_post_event_enabled
