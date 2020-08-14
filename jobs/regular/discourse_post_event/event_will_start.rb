af3e1595013e4023fb4fbe421fc3b08aef5b1f94
jjaffeux <j.jaffeux@gmail.com>
2020-08-15 01:14:22 +0200

    DEV: forces post stream refresh on event hooks

--- jobs/regular/discourse_post_event/event_will_start.rb
+++ jobs/regular/discourse_post_event/event_will_start.rb
@@ -7,7 +7,7 @@ module Jobs
     def execute(args)
       raise Discourse::InvalidParameters.new(:event_id) if args[:event_id].blank?
       event = DiscoursePostEvent::Event.find(args[:event_id])
-      event.publish_update!
+      MessageBus.publish("/topic/#{event.post.topic_id}", refresh_stream: true)
       DiscourseEvent.trigger(:discourse_post_event_event_will_start, event)
     end
   end
