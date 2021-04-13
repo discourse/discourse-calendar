66a0ed2ae92a12c7ab933059adf7dcefea964020
Dan Ungureanu <dan@ungureanu.me>
2021-04-13 18:39:52 +0300

    FIX: Handle multiple holidays in the same day
    If a user had two holidays in the same day, the calendar would show
    only of them, but the username would be shown twice. This ensures the
    calendar shows both holidays and the username only once.

--- spec/serializers/post_serializer_spec.rb
+++ spec/serializers/post_serializer_spec.rb
@@ -27,4 +27,24 @@ describe PostSerializer do
     expect(json[:post][:group_timezones]["admins"].count).to eq(1)
     expect(json[:post][:group_timezones]["trust_level_0"].count).to eq(2)
   end
+
+  it "groups calendar events correctly" do
+    user = Fabricate(:user)
+    user.upsert_custom_fields(::DiscourseCalendar::REGION_CUSTOM_FIELD => 'ar')
+
+    post = create_post(raw: "[calendar]\n[/calendar]")
+    SiteSetting.holiday_calendar_topic_id = post.topic.id
+
+    freeze_time Date.new(2021, 4, 1)
+    ::DiscourseCalendar::CreateHolidayEvents.new.execute({})
+
+    json = PostSerializer.new(post.reload, scope: Guardian.new).as_json
+    expect(json[:post][:calendar_details].map { |x| x[:name] }).to contain_exactly(
+      "Día del Veterano y de los Caídos en la Guerra de Malvinas, Viernes Santo",
+      "Día de la Revolución de Mayo",
+      "Feriado puente turístico",
+      "Día de la Independencia"
+    )
+    expect(json[:post][:calendar_details].map { |x| x[:usernames] }).to all (contain_exactly(user.username))
+  end
 end
