9ab1b76a1425304f3434b49db49cb4eec8919cf6
Blake Erickson <o.blakeerickson@gmail.com>
2022-06-15 10:05:42 -0600

    FIX: Stable core still needs a 6.1 migration
    The stable version of discourse is still on rails 6, so we can't use a
    version 7 migration here.
    
    See: https://meta.discourse.org/t/229985

--- db/post_migrate/20220613073844_unescape_event_name.rb
+++ db/post_migrate/20220613073844_unescape_event_name.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UnescapeEventName < ActiveRecord::Migration[7.0]
+class UnescapeEventName < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def up
