67b81b3d691437089aefb00d01995ecc5973780a
David Taylor <david@taylorhq.com>
2022-06-17 18:26:42 +0100

    DEV: Add hidden setting which makes holiday status public (#288)
    This is a temporary addition, pending a more radical refactoring of the holiday-emoji-flair system.

---
+++ spec/requests/site_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+describe "calendar site additions" do
+  let(:user) { Fabricate(:user) }
+  let(:admin) { Fabricate(:admin) }
+
+  before do
+    SiteSetting.calendar_enabled = true
+    DiscourseCalendar.users_on_holiday = [user.username]
+  end
+
+  it "includes users_on_holiday for staff only" do
+    get "/site.json"
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["users_on_holiday"]).to eq(nil)
+
+    sign_in(user)
+    get "/site.json"
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["users_on_holiday"]).to eq(nil)
+
+    sign_in(admin)
+    get "/site.json"
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["users_on_holiday"]).to eq([user.username])
+  end
+
+  it "makes it public when enabled" do
+    SiteSetting.holiday_calendar_users_public = true
+    get "/site.json"
+    expect(response.status).to eq(200)
+    expect(response.parsed_body["users_on_holiday"]).to eq([user.username])
+  end
+end
