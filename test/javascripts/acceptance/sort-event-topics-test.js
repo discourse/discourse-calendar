488d11602f6aaf8de31aee454ffc2c518df9dc81
Frank <frank3manuel@gmail.com>
2022-09-22 03:28:20 +0800

    JS test

---
+++ test/javascripts/acceptance/sort-event-topics-test.js
@@ -0,0 +1,22 @@
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
+import { test } from "qunit";
+import { visit } from "@ember/test-helpers";
+import { cloneJSON } from "discourse-common/lib/object";
+import CategoryFixtures from "discourse/tests/fixtures/category-fixtures";
+
+acceptance("Calendar - Disable sorting headers", function (needs) {
+  needs.user();
+  needs.pretender((server, helper) => {
+    const cardResponse = cloneJSON(CategoryFixtures["/c/1/show.json"]);
+    cardResponse.category.custom_fields["disable_topic_resorting"] = true;
+    server.get("/c/1/show.json", () => helper.response(cardResponse));
+  });
+
+  test("visiting a category page", async function (assert) {
+    await visit("/c/bug");
+    assert.ok(exists(".topic-list"), "The list of topics was rendered");
+    assert.ok(exists(".topic-list .topic-list-data"), "The headers were rendered");
+    assert.ok(!exists(".topic-list .sortable"), "The headers are not sortable");
+  });
+
+});
