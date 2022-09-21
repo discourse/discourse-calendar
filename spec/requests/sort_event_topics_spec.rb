0aeb06ab1ea1e2c7c32f8be3efd55cebd769cf20
Frank <frank3manuel@gmail.com>
2022-09-22 03:32:20 +0800

    Linting of spec

--- spec/requests/sort_event_topics_spec.rb
+++ spec/requests/sort_event_topics_spec.rb
@@ -13,18 +13,17 @@ RSpec.describe ListController do
   fab!(:post_2) { Fabricate(:post, topic: topic_2) }
   fab!(:post_event_2) { Fabricate(:event, name: "event2", post: post_2, original_starts_at: 2.days.from_now) }
 
-
   before do
     admin
     SiteSetting.calendar_enabled = true
     SiteSetting.sort_categories_by_event_start_date_enabled = true
   end
 
-  context '#sort_event_topics' do
+  describe '#sort_event_topics' do
     it 'gets topics in order of event_date if sort_topics_by_event_start_date is true' do
       category.custom_fields["sort_topics_by_event_start_date"] = true
       category.save
-  
+
       get "/c/#{category.slug}/#{category.id}/l/latest.json?ascending=false"
 
       topics = response.parsed_body["topic_list"]["topics"]
@@ -44,7 +43,7 @@ RSpec.describe ListController do
       expect(topics[0]["id"]).to eq(topic_2.id)
       expect(topics[1]["id"]).to eq(topic_1.id)
     end
-    
+
   end
-  
+
 end
