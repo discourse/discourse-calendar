52f2c32d956c07271769bb2436a819e31f2fc9f9
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-10-20 15:34:34 +0400

    Commit suggestion EMOJI = 'desert_island'
    Co-authored-by: Jarek Radosz <jradosz@gmail.com>

--- lib/holiday_status.rb
+++ lib/holiday_status.rb
@@ -2,7 +2,7 @@
 
 module DiscourseCalendar
   class HolidayStatus
-    EMOJI ||= 'desert_island'
+    EMOJI = 'desert_island'
 
     def self.is_holiday_status?(status)
       status.emoji == EMOJI && status.description == I18n.t("discourse_calendar.holiday_status.description")
