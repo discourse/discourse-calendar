10077ca904956005f9fa83c3d9fb124b59e8c47b
Selase Krakani <849886+s3lase@users.noreply.github.com>
2023-04-19 18:15:31 +0000

    DEV: Add Eid-Ul-Fir Holiday for Ghana (#402)
    One of two holidays with no fixed dates.
    See https://www.mint.gov.gh/declaration-of-monday-24th-april-2023-as-a-public-holiday/

--- vendor/holidays/test/defs/test_defs_gh.rb
+++ vendor/holidays/test/defs/test_defs_gh.rb
@@ -17,6 +17,8 @@ class GhDefinitionTests < Test::Unit::TestCase  # :nodoc:
 
     assert_equal "Independence Day", (Holidays.on(Date.civil(2022, 3, 7), [:gh], [:observed])[0] || {})[:name]
 
+    assert_equal "Eid-ul-Fitr", (Holidays.on(Date.civil(2023, 4, 24), [:gh], [:observed])[0] || {})[:name]
+
     assert_equal "May Day (Workers' Day)", (Holidays.on(Date.civil(2022, 5, 1), [:gh])[0] || {})[:name]
 
     assert_equal "African Union Day", (Holidays.on(Date.civil(2022, 5, 25), [:gh], [:informal])[0] || {})[:name]
