dc32b1f6a57eb14c712d4065e2328a59f08e4818
Kris <kris.aubuchon@discourse.org>
2023-07-31 14:56:23 -0400

    FIX: add outlet names, they're no longer passed in (#424)

--- assets/javascripts/connectors/before-topic-list-body/category-calendar.js
+++ assets/javascripts/connectors/before-topic-list-body/category-calendar.js
@@ -1,5 +1,7 @@
 export default {
   shouldRender(_, ctx) {
-    return ctx.siteSettings.calendar_categories_outlet === ctx.name;
+    return (
+      ctx.siteSettings.calendar_categories_outlet === "before-topic-list-body"
+    );
   },
 };
