df8a07c6011a0b7020c0b8a2292bbba327d677ab
Nat <natalie.tay@discourse.org>
2023-08-28 19:43:21 +0800

    Import required functions

--- test/javascripts/acceptance/sidebar-upcoming-events-item-test.js
+++ test/javascripts/acceptance/sidebar-upcoming-events-item-test.js
@@ -1,6 +1,6 @@
 import { test } from "qunit";
-import { visit } from "@ember/test-helpers";
-import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import { click, visit } from "@ember/test-helpers";
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("Discourse Calendar - hamburger action shown", function (needs) {
   needs.user();
