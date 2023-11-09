65669152011142cab3e2f7f51a98651a03b4dbf0
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2023-11-09 13:52:48 -0700

    DEV: Update Event Invitees modal to new Component API (#476)
    - Upgrade modal to new component based API
    - Remove redundant event status in RSVPed modal
    
    # Before
    <img width="593" alt="Screenshot 2023-11-09 at 12 28 03 PM" src="https://github.com/discourse/discourse-calendar/assets/50783505/c860c5b5-10b0-4b09-b25b-56bf5d6262e8">
    
    # After
    <img width="334" alt="Screenshot 2023-11-09 at 1 12 58 PM" src="https://github.com/discourse/discourse-calendar/assets/50783505/9a2132ae-179c-455c-8dda-65a060416c3c">

--- assets/javascripts/discourse/widgets/discourse-post-event.js
+++ assets/javascripts/discourse/widgets/discourse-post-event.js
@@ -1,5 +1,4 @@
 import EmberObject from "@ember/object";
-import { dasherize } from "@ember/string";
 import { routeAction } from "discourse/helpers/route-action";
 import { exportEntity } from "discourse/lib/export-csv";
 import showModal from "discourse/lib/show-modal";
@@ -8,12 +7,13 @@ import { escapeExpression } from "discourse/lib/utilities";
 import hbs from "discourse/widgets/hbs-compiler";
 import { createWidget } from "discourse/widgets/widget";
 import I18n from "I18n";
+import PostEventInvitees from "../components/modal/post-event-invitees";
 import cleanTitle from "../lib/clean-title";
 import { buildParams, replaceRaw } from "../lib/raw-event-helper";
 
 export default createWidget("discourse-post-event", {
   tagName: "div.discourse-post-event-widget",
-  services: ["dialog"],
+  services: ["dialog", "store", "modal", "currentUser", "siteSettings"],
 
   buildKey: (attrs) => `discourse-post-event-${attrs.id}`,
 
@@ -32,20 +32,17 @@ export default createWidget("discourse-post-event", {
   },
 
   showAllInvitees(params) {
-    const postId = params.postId;
-    const title = params.title || "title_invited";
-    const extraClass = params.extraClass || "invited";
-    const name = "discourse-post-event-invitees";
-
-    this.store.find("discourse-post-event-event", postId).then((eventModel) => {
-      showModal(name, {
-        model: eventModel,
-        title: `discourse_post_event.invitees_modal.${title}`,
-        modalClass: [`${dasherize(name).toLowerCase()}-modal`, extraClass].join(
-          " "
-        ),
+    this.store
+      .find("discourse-post-event-event", params.postId)
+      .then((eventModel) => {
+        this.modal.show(PostEventInvitees, {
+          model: {
+            event: eventModel,
+            title: params.title,
+            extraClass: params.extraClass,
+          },
+        });
       });
-    });
   },
 
   editPostEvent(postId) {
