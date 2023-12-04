39bd81580f2dd40f91cfbdee1e76ea39cd4760d4
Jarek Radosz <jradosz@gmail.com>
2023-12-04 17:49:13 +0100

    DEV: Fix flaky specs (#492)

--- spec/jobs/scheduled/create_holiday_events_spec.rb
+++ spec/jobs/scheduled/create_holiday_events_spec.rb
@@ -33,13 +33,13 @@ describe DiscourseCalendar::CreateHolidayEvents do
     freeze_time Time.zone.local(2019, 8, 1)
     DiscourseCalendar::CreateHolidayEvents.new.execute(nil)
 
-    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to eq(
+    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to match_array(
       [
-        ["fr", "Assomption", "2019-08-15", frenchy.username],
-        ["fr", "Toussaint", "2019-11-01", frenchy.username],
-        ["fr", "Armistice 1918", "2019-11-11", frenchy.username],
-        ["fr", "Noël", "2019-12-25", frenchy.username],
-        ["fr", "Jour de l'an", "2020-01-01", frenchy.username],
+        ["fr", "Assomption", Date.parse("2019-08-15"), frenchy.username],
+        ["fr", "Toussaint", Date.parse("2019-11-01"), frenchy.username],
+        ["fr", "Armistice 1918", Date.parse("2019-11-11"), frenchy.username],
+        ["fr", "Noël", Date.parse("2019-12-25"), frenchy.username],
+        ["fr", "Jour de l'an", Date.parse("2020-01-01"), frenchy.username],
       ],
     )
   end
@@ -50,11 +50,11 @@ describe DiscourseCalendar::CreateHolidayEvents do
     DiscourseCalendar::CreateHolidayEvents.new.execute(nil)
 
     # The "Australia Day" is always observed on a Monday
-    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to eq(
+    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to match_array(
       [
-        ["au", "Australia Day", "2020-01-27", aussie.username],
-        ["au", "Good Friday", "2020-04-10", aussie.username],
-        ["au", "Easter Monday", "2020-04-13", aussie.username],
+        ["au", "Australia Day", Date.parse("2020-01-27"), aussie.username],
+        ["au", "Good Friday", Date.parse("2020-04-10"), aussie.username],
+        ["au", "Easter Monday", Date.parse("2020-04-13"), aussie.username],
       ],
     )
   end
@@ -65,13 +65,13 @@ describe DiscourseCalendar::CreateHolidayEvents do
     DiscourseCalendar::CreateHolidayEvents.new.execute(nil)
 
     # The "Fête Nationale" is on July 14th but it's on a Sunday in 2019
-    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to eq(
+    expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to match_array(
       [
-        ["fr", "Assomption", "2019-08-15", frenchy.username],
-        ["fr", "Toussaint", "2019-11-01", frenchy.username],
-        ["fr", "Armistice 1918", "2019-11-11", frenchy.username],
-        ["fr", "Noël", "2019-12-25", frenchy.username],
-        ["fr", "Jour de l'an", "2020-01-01", frenchy.username],
+        ["fr", "Assomption", Date.parse("2019-08-15"), frenchy.username],
+        ["fr", "Toussaint", Date.parse("2019-11-01"), frenchy.username],
+        ["fr", "Armistice 1918", Date.parse("2019-11-11"), frenchy.username],
+        ["fr", "Noël", Date.parse("2019-12-25"), frenchy.username],
+        ["fr", "Jour de l'an", Date.parse("2020-01-01"), frenchy.username],
       ],
     )
   end
@@ -166,11 +166,11 @@ describe DiscourseCalendar::CreateHolidayEvents do
       freeze_time Time.zone.local(2019, 7, 1)
       DiscourseCalendar::CreateHolidayEvents.new.execute(nil)
 
-      expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to eq(
+      expect(CalendarEvent.pluck(:region, :description, :start_date, :username)).to match_array(
         [
-          ["fr", "Armistice 1918", "2019-11-11", frenchy.username],
-          ["fr", "Noël", "2019-12-25", frenchy.username],
-          ["fr", "Jour de l'an", "2020-01-01", frenchy.username],
+          ["fr", "Armistice 1918", Date.parse("2019-11-11"), frenchy.username],
+          ["fr", "Noël", Date.parse("2019-12-25"), frenchy.username],
+          ["fr", "Jour de l'an", Date.parse("2020-01-01"), frenchy.username],
         ],
       )
     end
