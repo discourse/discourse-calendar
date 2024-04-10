19f1d8e9da60b09486be1664524cef118b576f5b
Selase Krakani <849886+s3lase@users.noreply.github.com>
2024-04-10 12:08:27 +0000

    DEV: Add upcoming Eid-Ul-Fitr holiday for Ghana (#558)
    Switched to using a function for Eid-Ul-Fitr to make future updates seamless.
    
    See https://www.mint.gov.gh/declaration-of-thursday-11th-april-2024-as-a-statutory-public-holiday/

--- vendor/holidays/test/defs/test_defs_gh.rb
+++ vendor/holidays/test/defs/test_defs_gh.rb
@@ -18,6 +18,7 @@ class GhDefinitionTests < Test::Unit::TestCase  # :nodoc:
     assert_equal "Independence Day", (Holidays.on(Date.civil(2022, 3, 7), [:gh], [:observed])[0] || {})[:name]
 
     assert_equal "Eid-ul-Fitr", (Holidays.on(Date.civil(2023, 4, 24), [:gh], [:observed])[0] || {})[:name]
+assert_equal "Eid-ul-Fitr", (Holidays.on(Date.civil(2024, 4, 11), [:gh], [:observed])[0] || {})[:name]
 
     assert_equal "May Day (Workers' Day)", (Holidays.on(Date.civil(2022, 5, 1), [:gh])[0] || {})[:name]
 
