6168eb54af1aafee5e75c52c24b1438a3b415373
Régis Hanol <regis@hanol.fr>
2024-05-14 09:42:43 +0200

    FEATURE: closed events
    Adds support for "closed" events instead of just relying on the start & end dates.
    
    An event might now be "expired" when it happened in the past.
    
    An event might now also be "closed", if for whatever reasons, the author decides to "cancel" it.
    
    Context - https://meta.discourse.org/t/when-closing-event-it-moves-it-to-todays-date-time/307292

---
+++ spec/system/post_event_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+describe "Post event", type: :system do
+  fab!(:admin)
+  let(:composer) { PageObjects::Components::Composer.new }
+
+  before do
+    SiteSetting.calendar_enabled = true
+    SiteSetting.discourse_post_event_enabled = true
+    sign_in(admin)
+    visit "/new-topic"
+  end
+
+  it "can create, close, and open an event" do
+    title = "My upcoming l33t event"
+    tomorrow = (Time.zone.now + 1.day).strftime("%Y-%m-%d")
+
+    composer.fill_title(title)
+
+    composer.fill_content <<~MD
+      [event start="#{tomorrow} 13:37" status="public"]
+      [/event]
+    MD
+
+    composer.submit
+
+    expect(page).to have_content(title)
+
+    page.find("#more-dropdown").click
+    page.find(".item-closeEvent").click
+    page.find("#dialog-holder .btn-primary").click
+
+    expect(page).to have_css(".discourse-post-event .status-and-creators .status.closed")
+
+    page.find("#more-dropdown").click
+    page.find(".item-openEvent").click
+    page.find("#dialog-holder .btn-primary").click
+
+    expect(page).to have_css(".discourse-post-event .status-and-creators .status.public")
+  end
+end
