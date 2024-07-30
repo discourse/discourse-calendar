a505285d1e607cf95ea3128e7664ca6e3f797ad5
Lhc_fl <linca@discourse.org>
2024-07-30 15:10:16 +0800

    forgot a file 🥲

---
+++ assets/javascripts/discourse/lib/calendar-locale.js
@@ -0,0 +1,15 @@
+import I18n from "I18n";
+
+export function getCurrentBcp47Locale() {
+  return I18n.currentLocale().replace("_", "-");
+}
+
+export function getCalendarButtonsText() {
+  return {
+    today: I18n.t("discourse_calendar.toolbar_button.today"),
+    month: I18n.t("discourse_calendar.toolbar_button.month"),
+    week: I18n.t("discourse_calendar.toolbar_button.week"),
+    day: I18n.t("discourse_calendar.toolbar_button.day"),
+    list: I18n.t("discourse_calendar.toolbar_button.list"),
+  };
+}
