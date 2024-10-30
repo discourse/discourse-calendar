0b20cd7eaa551bc13082652b9d74ceff64b60ba6
Jarek Radosz <jradosz@gmail.com>
2024-10-30 21:51:22 +0100

    DEV: Convert helpers to plain functions
    (`format-event-name` was unused)

--- assets/javascripts/discourse/helpers/format-event-name.js
+++ assets/javascripts/discourse/helpers/format-event-name.js
@@ -1,7 +1,3 @@
-import { htmlHelper } from "discourse-common/lib/helpers";
-
 export function formatEventName(event) {
   return event.name || event.post.topic.title;
 }
-
-export default htmlHelper((event) => formatEventName(event));
