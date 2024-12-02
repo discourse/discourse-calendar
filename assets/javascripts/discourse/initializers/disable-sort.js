026d755c78b892d257a3275d77d4bfb010ec0cb5
Jarek Radosz <jradosz@gmail.com>
2024-12-02 14:40:53 +0100

    DEV: Use `topic-list-header-sortable-column` transformer (#651)

--- assets/javascripts/discourse/initializers/disable-sort.js
+++ assets/javascripts/discourse/initializers/disable-sort.js
@@ -1,30 +1,46 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default {
   name: "disable-sort",
-  initialize() {
+
+  initialize(container) {
     withPluginApi("0.8", (api) => {
-      api.modifyClass(
-        "component:topic-list",
-        (Superclass) =>
-          class extends Superclass {
-            @discourseComputed(
-              "category",
-              "siteSettings.disable_resorting_on_categories_enabled"
-            )
-            sortable(category, disable_resorting_on_categories_enabled) {
-              let disableSort = false;
-              if (
-                disable_resorting_on_categories_enabled &&
-                category?.custom_fields
-              ) {
-                disableSort =
-                  !!category.custom_fields["disable_topic_resorting"];
+      // TODO: cvx - remove after the glimmer topic list transition
+      withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
+        api.modifyClass(
+          "component:topic-list",
+          (Superclass) =>
+            class extends Superclass {
+              @discourseComputed(
+                "category",
+                "siteSettings.disable_resorting_on_categories_enabled"
+              )
+              sortable(category, disable_resorting_on_categories_enabled) {
+                const disableSort =
+                  disable_resorting_on_categories_enabled &&
+                  !!category?.custom_fields?.disable_topic_resorting;
+
+                return super.sortable && !disableSort;
               }
-              return !!this.changeSort && !disableSort;
             }
+        );
+      });
+
+      api.registerValueTransformer(
+        "topic-list-header-sortable-column",
+        ({ value, context }) => {
+          if (!value) {
+            return value;
           }
+
+          const siteSettings = container.lookup("service:site-settings");
+          return !(
+            siteSettings.disable_resorting_on_categories_enabled &&
+            context.category?.custom_fields?.disable_topic_resorting
+          );
+        }
       );
     });
   },
