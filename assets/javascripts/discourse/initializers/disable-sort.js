6dfc5837a804ff370555061f29990f7e2096be7c
David Taylor <david@taylorhq.com>
2024-12-02 11:38:53 +0000

    DEV: Update modifyClass calls to native class syntax

--- assets/javascripts/discourse/initializers/disable-sort.js
+++ assets/javascripts/discourse/initializers/disable-sort.js
@@ -5,24 +5,27 @@ export default {
   name: "disable-sort",
   initialize() {
     withPluginApi("0.8", (api) => {
-      api.modifyClass("component:topic-list", {
-        pluginId: "discourse-calendar",
-
-        @discourseComputed(
-          "category",
-          "siteSettings.disable_resorting_on_categories_enabled"
-        )
-        sortable(category, disable_resorting_on_categories_enabled) {
-          let disableSort = false;
-          if (
-            disable_resorting_on_categories_enabled &&
-            category?.custom_fields
-          ) {
-            disableSort = !!category.custom_fields["disable_topic_resorting"];
+      api.modifyClass(
+        "component:topic-list",
+        (Superclass) =>
+          class extends Superclass {
+            @discourseComputed(
+              "category",
+              "siteSettings.disable_resorting_on_categories_enabled"
+            )
+            sortable(category, disable_resorting_on_categories_enabled) {
+              let disableSort = false;
+              if (
+                disable_resorting_on_categories_enabled &&
+                category?.custom_fields
+              ) {
+                disableSort =
+                  !!category.custom_fields["disable_topic_resorting"];
+              }
+              return !!this.changeSort && !disableSort;
+            }
           }
-          return !!this.changeSort && !disableSort;
-        },
-      });
+      );
     });
   },
 };
