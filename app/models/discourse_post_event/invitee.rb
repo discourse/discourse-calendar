fdf3ad927744a9dbb826cc46e489cca8ad469044
Juan David Martínez Cubillos <jd.martinez1062@gmail.com>
2025-01-24 12:03:46 -0500

    DEV: Add DiscourseEvent triggers to Invitee creation and update (#683)
    * DEV: Add DiscourseEvent triggers to Invitee creation and update
    
    * undid removed code
    
    * remove not longer used file
    
    * change trigger name

--- app/models/discourse_post_event/invitee.rb
+++ app/models/discourse_post_event/invitee.rb
@@ -22,6 +22,7 @@ module DiscoursePostEvent
         Invitee.create!(status: Invitee.statuses[status.to_sym], post_id: post_id, user_id: user_id)
       invitee.event.publish_update!
       invitee.update_topic_tracking!
+      DiscourseEvent.trigger(:discourse_calendar_post_event_invitee_status_changed, invitee)
       invitee
     rescue ActiveRecord::RecordNotUnique
       # do nothing in case multiple new attendances would be created very fast
@@ -34,6 +35,7 @@ module DiscoursePostEvent
       self.update(status: new_status)
       self.event.publish_update!
       self.update_topic_tracking! if status_changed
+      DiscourseEvent.trigger(:discourse_calendar_post_event_invitee_status_changed, self)
       self
     end
 
