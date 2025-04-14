4edac6a9872fbf37de083464b452d775ee413240
David Taylor <david@taylorhq.com>
2025-04-14 12:00:22 +0100

    DEV: Remove legacy topic-list code

--- assets/javascripts/discourse/initializers/disable-sort.js
+++ assets/javascripts/discourse/initializers/disable-sort.js
@@ -1,5 +1,3 @@
-import discourseComputed from "discourse/lib/decorators";
-import { withSilencedDeprecations } from "discourse/lib/deprecated";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
@@ -7,27 +5,6 @@ export default {
 
   initialize(container) {
     withPluginApi("0.8", (api) => {
-      // TODO: cvx - remove after the glimmer topic list transition
-      withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
-        api.modifyClass(
-          "component:topic-list",
-          (Superclass) =>
-            class extends Superclass {
-              @discourseComputed(
-                "category",
-                "siteSettings.disable_resorting_on_categories_enabled"
-              )
-              sortable(category, disable_resorting_on_categories_enabled) {
-                const disableSort =
-                  disable_resorting_on_categories_enabled &&
-                  !!category?.custom_fields?.disable_topic_resorting;
-
-                return super.sortable && !disableSort;
-              }
-            }
-        );
-      });
-
       api.registerValueTransformer(
         "topic-list-header-sortable-column",
         ({ value, context }) => {
